18155c4238cbf40d4ab6f92b0ef92fe68c4176e3
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2021-03-23 22:32:49 -0400

    Dilation and Erosion

--- ColorChangeTest/color_change_test.py
+++ ColorChangeTest/color_change_test.py
@@ -1,6 +1,5 @@
 import cv2
-from PIL import Image
-import matplotlib.pyplot as plt
+import numpy as np
 
 
 def cv_show(img, s):
@@ -11,7 +10,7 @@ def cv_show(img, s):
 
 img = cv2.imread('./MAtest01.png')
 print(img.shape)
-cv_show(img, "origin")
+cv_show(img, "Origin")
 
 width = img.shape[0]
 height = img.shape[1]
@@ -29,35 +28,17 @@ for i in range(0, width):
         else:
             img[i, j] = [0, 0, 0]
 
-cv_show(img, "color_change")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+print(img.shape)
+cv_show(img, "Extraction")
 
+kernel = np.ones((4, 4), dtype=np.uint8)
+img = cv2.dilate(img, kernel, iterations=1)
+print(img.shape)
+cv_show(img, "Dilation ")
 
-# img = Image.open("./MAtest01.png")
-# img_origin = img
-# print(img_origin.size)
-# plt.imshow(img_origin)
-# plt.show()
-#
-# width = img.size[0]
-# height = img.size[1]
-#
-# color_range = [[130, 170], [1, 90], [1, 55]]
-#
-# for i in range(0, width):
-#     for j in range(0, height):
-#         data = (img.getpixel((i, j)))
-#
-#         if color_range[0][0] <= data[0] <= color_range[0][1] \
-#                 and color_range[1][0] <= data[1] <= color_range[1][1] \
-#                 and color_range[2][0] <= data[2] <= color_range[2][1]:
-#             img.putpixel((i, j), (255, 255, 255))
-#         else:
-#             img.putpixel((i, j), (0, 0, 0))
-#
-# cv_show(img)
-#
-# img = img.convert("LA")
-# plt.imshow(img)
-# plt.show()
-
+kernel = np.ones((3, 3), dtype=np.uint8)
+img = cv2.erode(img, kernel, iterations=1)
+print(img.shape)
+cv_show(img, "Erosion")
 
