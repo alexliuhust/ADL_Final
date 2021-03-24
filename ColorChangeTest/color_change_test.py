431bae0a8276a370a6b66f0ae6b9f86adb2b5c81
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2021-03-23 22:00:42 -0400

    MA test 01

--- ColorChangeTest/color_change_test.py
+++ ColorChangeTest/color_change_test.py
@@ -1,24 +1,29 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 
-
-i = 1
-j = 1
-img = Image.open("./test01.png")
-print(img.size)
-plt.imshow(img)
+img = Image.open("./MAtest01.png")
+img_origin = img
+print(img_origin.size)
+plt.imshow(img_origin)
 plt.show()
 
 width = img.size[0]
 height = img.size[1]
-for i in range(0,width):
+
+color_range = [[130, 170], [1, 90], [1, 55]]
+
+for i in range(0, width):
     for j in range(0, height):
         data = (img.getpixel((i, j)))
 
-        if data[0] >= 170 and data[1] >= 170 and data[2] >= 170:
-            img.putpixel((i, j), (234, 53, 57))
+        if color_range[0][0] <= data[0] <= color_range[0][1] \
+                and color_range[1][0] <= data[1] <= color_range[1][1] \
+                and color_range[2][0] <= data[2] <= color_range[2][1]:
+            img.putpixel((i, j), (255, 255, 255))
+        else:
+            img.putpixel((i, j), (0, 0, 0))
 
-img = img.convert("RGB")
+img = img.convert("LA")
 plt.imshow(img)
 plt.show()
 
