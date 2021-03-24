d04b3b95dd6bdd9c315c4c7a66e0b57d23d1d4c2
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2021-03-23 22:20:57 -0400

    Use cv2

--- ColorChangeTest/color_change_test.py
+++ ColorChangeTest/color_change_test.py
@@ -1,30 +1,63 @@
+import cv2
 from PIL import Image
 import matplotlib.pyplot as plt
 
-img = Image.open("./MAtest01.png")
-img_origin = img
-print(img_origin.size)
-plt.imshow(img_origin)
-plt.show()
 
-width = img.size[0]
-height = img.size[1]
+def cv_show(img, s):
+    cv2.imshow(s, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-color_range = [[130, 170], [1, 90], [1, 55]]
+
+img = cv2.imread('./MAtest01.png')
+print(img.shape)
+cv_show(img, "origin")
+
+width = img.shape[0]
+height = img.shape[1]
+
+color_range = [[1, 55], [1, 90], [130, 170]]  # This is BGR system
 
 for i in range(0, width):
     for j in range(0, height):
-        data = (img.getpixel((i, j)))
+        data = (img[i, j])
 
         if color_range[0][0] <= data[0] <= color_range[0][1] \
                 and color_range[1][0] <= data[1] <= color_range[1][1] \
                 and color_range[2][0] <= data[2] <= color_range[2][1]:
-            img.putpixel((i, j), (255, 255, 255))
+            img[i, j] = [255, 255, 255]
         else:
-            img.putpixel((i, j), (0, 0, 0))
+            img[i, j] = [0, 0, 0]
 
-img = img.convert("LA")
-plt.imshow(img)
-plt.show()
+cv_show(img, "color_change")
+
+
+# img = Image.open("./MAtest01.png")
+# img_origin = img
+# print(img_origin.size)
+# plt.imshow(img_origin)
+# plt.show()
+#
+# width = img.size[0]
+# height = img.size[1]
+#
+# color_range = [[130, 170], [1, 90], [1, 55]]
+#
+# for i in range(0, width):
+#     for j in range(0, height):
+#         data = (img.getpixel((i, j)))
+#
+#         if color_range[0][0] <= data[0] <= color_range[0][1] \
+#                 and color_range[1][0] <= data[1] <= color_range[1][1] \
+#                 and color_range[2][0] <= data[2] <= color_range[2][1]:
+#             img.putpixel((i, j), (255, 255, 255))
+#         else:
+#             img.putpixel((i, j), (0, 0, 0))
+#
+# cv_show(img)
+#
+# img = img.convert("LA")
+# plt.imshow(img)
+# plt.show()
 
 
