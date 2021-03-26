5dfef9a648fe3cb19a2b7148d65a4e3b40d94355
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2021-03-25 22:42:55 -0400

    States' unique pattern 02

--- ColorChangeTest/maintest.py
+++ ColorChangeTest/maintest.py
@@ -1,8 +1,13 @@
 from color_change_test import get_chars
 
-get_chars("MA01")
-get_chars("MA02")
-get_chars("MA03")
-get_chars("MA04")
-get_chars("MA05")
-get_chars("MA06")
+# get_chars("MA01")
+# get_chars("MA02")
+# get_chars("MA03")
+# get_chars("MA04")
+# get_chars("MA05")
+# get_chars("MA06")
+
+
+get_chars("MD01")
+get_chars("MD02")
+get_chars("MD03")
