fc7c8bd7e3f5347dcee48169eb8790ed8569ecb8
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2021-04-19 19:47:45 -0400

    Extract chars by color: NY

--- ColorChangeTest/MainTest.py
+++ ColorChangeTest/MainTest.py
@@ -1,8 +1,8 @@
 from ExtractCharsByColor import get_chars
 
-state = 'CA'
+state = 'NY'
 
-for i in range(7, 19):
+for i in range(3, 19):
     if i <= 9:
         file_name = state + "0" + str(i) + ".png"
     else:
