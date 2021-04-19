b45da7f9b481ea3bcbd8e16b6b4d91a9d92b2c78
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2021-04-19 16:26:34 -0400

    Extract chars by color: GA, NJ

--- ColorChangeTest/MainTest.py
+++ ColorChangeTest/MainTest.py
@@ -1,6 +1,13 @@
 from ExtractCharsByColor import get_chars
 
-get_chars("MA01.png")
-get_chars("MA02.png")
-get_chars("MA03.png")
-# get_chars("MA04")
+state = 'NJ'
+
+for i in range(1, 11):
+    if i <= 9:
+        file_name = state + "0" + str(i) + ".png"
+    else:
+        file_name = state + str(i) + ".png"
+    get_chars(file_name)
+
+
+
--- ColorChangeTest/StateColorRange.py
+++ ColorChangeTest/StateColorRange.py
@@ -1,10 +1,11 @@
+ranges = {
+    "MA": [[1, 165], [1, 145], [130, 240]],
+    "MD": [[0, 122], [0, 81], [0, 65]],
+    "GA": [[0, 122], [0, 81], [0, 65]],
+    "NJ": [[0, 93], [0, 88], [0, 82]],
+}
+
 
 def get_color_range(file_name):
     state = file_name[0:2]
-    if state == "MA":
-        return [[1, 165], [1, 145], [130, 240]]
-    elif state == "MD" or state == "NE":
-        return [[0, 38], [0, 38], [0, 38]]
-    elif state == "MS":
-        return [[38, 130], [21, 78], [10, 60]]
-
+    return ranges[state]
